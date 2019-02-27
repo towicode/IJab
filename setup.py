df3bd31bac3378394542d8ad5740f592f496ab3b
todd wickizer <toddwickizer@gmail.com>
2019-02-27 22:52:57 +0900

    bump version numbers again for consistency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 setuptools.setup(
     name='jupyterlab_irods',
     description='A Jupyter Notebook server extension which acts as a backend for IRODS requests.',
-    version='2.0.4',
+    version='2.0.5',
     packages=setuptools.find_packages(),
     author          = 'Todd Wickizer',
     author_email    = 'jupyter@googlegroups.com',
