2e2060818618048ba1dd85f3823a179c90490767
Your Name <you@example.com>
2019-12-03 15:43:35 -0700

    add python dist

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 setuptools.setup(
     name='jupyterlab_irods',
     description='A Jupyter Notebook server extension which acts as a backend for IRODS requests.',
-    version='3.0.0',
+    version='3.0.1',
     packages=setuptools.find_packages(),
     author          = 'Todd Wickizer',
     author_email    = 'jupyter@googlegroups.com',
